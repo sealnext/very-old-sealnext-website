3bf113d54b50a78dd228aeb8358b7675043a913b
Ovidiu Bachmatchi <ovidiubachmatchi@gmail.com>
2023-07-23 01:41:02 +0300

    push

---
+++ src/components/Top.svelte
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<div class="flex justify-between px-6 sm:px-20">
+    <div class="logo">SEALNEXT</div>
+    <div class="flex justify-">
+      <a href="#about">About</a>
+      <a href="#reviews">Reviews</a>
+      <a href="#services">Services</a>
+      <a href="#contact">Contact</a>
+    </div>
+</div>
