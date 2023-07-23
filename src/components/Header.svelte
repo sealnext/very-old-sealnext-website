682598318677982d351a9902fafd44a2e43fae0a
Ovidiu Bachmatchi <ovidiubachmatchi@gmail.com>
2023-07-23 14:05:23 +0300

    separated files

---
+++ src/components/Header.svelte
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<div class="header flex flex-row justify-between shadow-md items-center">
+        <div class="m-5 min font-medium text-lg">SEALNEXT</div>
+        <div class="hidden md:block m-5 justify-evenly">
+            <a href="#about" class="mx-2">About</a>
+            <a href="#reviews" class="mx-2">Reviews</a>
+            <a href="#services" class="mx-2">Services</a>
+            <a href="#contact" class="mx-2">Contact</a>
+        </div>
+        <div class="block md:hidden m-5">
+            <button>
+                <svg fill="currentColor" class="w-4 h-4 text-gray-800" width="24" height="24" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <title>Menu</title>
+                    <path class="hidden" fill-rule="evenodd" clip-rule="evenodd" d="M18.278 16.864a1 1 0 01-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 01-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 011.414-1.414l4.829 4.828 4.828-4.828a1 1 0 111.414 1.414l-4.828 4.829 4.828 4.828z"></path>
+                    <path class="" fill-rule="evenodd" d="M4 5h16a1 1 0 010 2H4a1 1 0 110-2zm0 6h16a1 1 0 010 2H4a1 1 0 010-2zm0 6h16a1 1 0 010 2H4a1 1 0 010-2z"></path>
+                  </svg>
+            </button>
+        </div>
+</div>
+
+<style>
+    .header {
+        font-family: InterVariable,Inter,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,"Apple Color Emoji","Segoe UI Emoji",Segoe UI Symbol,"Noto Color Emoji";
+    }
+</style>
