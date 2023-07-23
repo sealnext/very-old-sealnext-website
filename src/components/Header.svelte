5eea1d8d6b2a09d8adf49334c3031201e1c0dd8f
Ovidiu Bachmatchi <ovidiubachmatchi@gmail.com>
2023-07-23 21:09:40 +0300

    changes small

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,13 +1,13 @@
-<header class="text-gray-600 body-font">
-  <div class="flex flex-col flex-wrap items-center w-full p-5 mx-auto md:flex-row">
-    <a class="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0">
+<header class="mx-auto text-gray-600 body-font">
+  <div class="flex flex-col flex-wrap items-center w-full p-5 px-16 mx-auto md:px-5 lg:px-16 md:flex-row">
+    <a href="#" class="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0">
       <span class="ml-3 text-xl">SEALNEXT</span>
     </a>
-    <nav class="flex flex-wrap items-center justify-center text-base md:ml-auto">
-      <a class="mr-5 hover:text-gray-900">About</a>
-      <a class="mr-5 hover:text-gray-900">Reviews</a>
-      <a class="mr-5 hover:text-gray-900">Services</a>
-      <a class="mr-5 hover:text-gray-900">Contact</a>
+    <nav class="flex flex-wrap items-center justify-center text-2xl md:text-xl md:ml-auto">
+      <a href="#about" class="mr-5 hover:text-gray-900">About</a>
+      <a href="#reviews" class="mr-5 hover:text-gray-900">Reviews</a>
+      <a href="#services" class="mr-5 hover:text-gray-900">Services</a>
+      <a href="#contact" class="mr-5 hover:text-gray-900">Contact</a>
     </nav>
   </div>
 </header>
--- src/components/HeroSection.svelte
+++ src/components/HeroSection.svelte
@@ -1,19 +1,19 @@
 <section class="body-font">
-  <div class="flex flex-col items-center justify-center px-5 py-24 mx-auto w-fit md:flex-row">
-    <div class="flex flex-col items-center mb-16 text-center md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0">
-      <h1 class="mb-4 text-6xl font-bold title-font max-xl:text-5xl">
+  <div class="flex flex-col items-center justify-center px-5 py-16 mx-auto w-fit md:flex-row">
+    <div class="flex flex-col items-center mb-16 text-center md:w-1/2 md:items-start md:text-left md:mb-0">
+      <h1 class="mb-4 text-6xl font-bold text-clip title-font max-xl:text-5xl">
         Leading Your Business
         <br class="hidden lg:inline-block" />into the Digital Future
       </h1>
       <p class="mb-4 text-3xl font-normal leading-snug max-xl:text-xl">Developing high-quality, innovative software solutions to accelerate your business growth and efficiency.</p>
 
-      <div class="flex justify-center text-xl font-medium">
-        <button class="px-2 py-3 text-white bg-black rounded-xl w-44 hover:bg-gray-800">Find Out How</button>
-        <button class="px-2 py-3 ml-4 border-2 border-black rounded-xl w-44 ml- hover:bg-gray-200">Contact Us</button>
+      <div class="flex flex-col flex-wrap content-center justify-center w-full text-xl font-medium md:justify-start md:items-start md:flex-row">
+        <button class="w-5/6 py-3 mb-3 text-white bg-black border-2 border-black rounded-xl md:w-40 md:h-16 hover:bg-gray-800">Find Out How</button>
+        <button class="w-5/6 py-3 border-2 border-black md:ml-4 md:h-16 rounded-xl md:w-40 hover:bg-gray-200">Contact Us</button>
       </div>
     </div>
 
-    <div class="w-5/6 lg:max-w-lg lg:w-full md:w-1/2">
+    <div class="w-2/4 lg:max-w-lg lg:w-full md:w-1/2">
       <img class="object-cover object-center rounded" alt="logo" src="big_seal.png" />
     </div>
   </div>
