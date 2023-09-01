8dc7eb8a3860584a982ee0629358ead42e361749
Ovidiu Bachmatchi <ovidiubachmatchi@gmail.com>
2023-09-01 10:52:52 +0300

    initial commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,13 @@
-import { vitePreprocess } from "@astrojs/svelte";
+import adapter from '@sveltejs/adapter-auto';
 
-export default {
-  preprocess: vitePreprocess(),
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
+
+export default config;
